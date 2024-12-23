6b4e341ed1a58205ab861866d51beda516462743
tjtc2024 <tjtc2024@gmail.com>
2024-12-23 22:47:10 +0900

    オンライン観望会(西川公園)

---
+++ stargazing/nishigawa_2024/slide.js
@@ -0,0 +1,26 @@
+
+let button_play = document.getElementById('play');
+let button_stop = document.getElementById('stop');
+
+const images = document.querySelectorAll('img');
+let number = 0;
+let interval = 5000;
+
+let play = false;
+
+function changeImage() {
+  if (play) {
+    images[number].classList.remove('active');
+    number = (number + 1) % images.length;
+    images[number].classList.add('active');
+  }
+}
+
+function playButton() { play = true; }
+function stopButton() { play = false; }
+
+
+button_play.addEventListener('click', playButton);
+button_stop.addEventListener('click', stopButton);
+
+setInterval(changeImage, interval);
