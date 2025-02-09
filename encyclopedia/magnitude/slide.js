b2c6b03ad90da67db116cefd20a859d773ec3508
tjtc2024 <tjtc2024@gmail.com>
2025-02-09 22:50:40 +0900

    天文百科事典更新

---
+++ encyclopedia/magnitude/slide.js
@@ -0,0 +1,32 @@
+
+const div = document.getElementById('conv_json');
+
+const loadHtml = (jsonObj) => {
+    
+    let htmlBody = "<h2>" + jsonObj.title + "</h2>";
+    const sections = jsonObj.sections;
+
+    for (let n = 0; n < sections.length; n++) {
+        if (sections[n].img) {
+            htmlBody += "<p><img src=\"" + sections[n].img + "\"></p>";
+        }
+
+        const lines = sections[n].lines
+        //htmlBody += "<p>" + sections[n].lines[0].line + "</p>";
+        for (let m = 0; m < lines.length; m++) {
+            htmlBody += "<p>" + lines[m] + "</p>";
+        }
+    }
+    div.innerHTML = htmlBody;
+} 
+
+
+const loadObj =
+    (url) => fetch(url)
+        .then((response) => { return response.json(); })
+        .then((result) => {loadHtml(result);})
+        .catch((e) => {})
+
+
+// ここにファイル名を指定
+loadObj('slide.json');
