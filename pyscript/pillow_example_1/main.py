6c83b6e7644c1a364857fc6ff10dfcc8f46fb1ad
tjtc2024 <tjtc2024@gmail.com>
2025-02-17 01:44:17 +0900

    autocontrast例

--- pyscript/pillow_example_1/main.py
+++ pyscript/pillow_example_1/main.py
@@ -1,23 +1,13 @@
 from pyscript import display
-from PIL import Image
+from PIL import Image, ImageOps
 import matplotlib.pyplot as plt
 import numpy as np
 # ライブラリのインポート宣言 --- (*3)
-#min = 140
-#tone_curve = [int(255 * (n - min) / (255 - min)) if n > min else 0 for n in range(0,256)]
+min = 140
 
 # トーンカーブ補正
 im = Image.open("diamond_gs1.jpg")
-#diamond_toned = ImageOps.autocontrast(diamond, curve=tone_curve)
-im_toned = np.asarray(im)
-
-#w,h = im.size
-
-#for y in range(0,h):
-#    for x in range(0,w):
-#        c_pre = im[y][x]
-#        im_toned[y][x] = 255 * (c_pre - 130) / (255 - 130) if c_pre > 130 else 0
-
+im_toned = ImageOps.autocontrast(im, 0.5)
 
 fig, ax = plt.subplots()
 ax.imshow(im_toned, cmap="gray")
