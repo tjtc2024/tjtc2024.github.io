dfae3839374d05663545676a3b06f30d93cc4da0
tjtc2024 <tjtc2024@gmail.com>
2025-05-10 00:47:12 +0900

    相対性理論修正

--- encyclopedia/relativity_and_blackhole/slide.js
+++ encyclopedia/relativity_and_blackhole/slide.js
@@ -1,15 +1,24 @@
 
 const imgpage = document.getElementById("imgpage");
 
+const pages = [
+    "page0.png",
+    "page1.png",
+    "page2.png",
+    "page3.png",
+    "page4.png"
+];
+
+
 let idx = 0;
 const nextPage = () => {
-    idx = (idx < 9) ? idx+1 : 0;
-    imgpage.innerHTML = "<img src=\"page" + idx.toString() + ".png\">";
+    idx = (idx < pages.length-1) ? idx+1 : 0;
+    imgpage.innerHTML = "<img src=\"" + pages[idx] + "\">";
 }
 
 const prevPage = () => {
-    idx = (idx > 0) ? idx-1 : 9;
-    imgpage.innerHTML = "<img src=\"page" + idx.toString() + ".png\">";
+    idx = (idx > 0) ? idx-1 : pages.length-1;
+    imgpage.innerHTML = "<img src=\"" + pages[idx] + "\">";
 }
 
 
